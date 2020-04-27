35dbeef01a710088b766c91add04afeb700b1c25
Ben Capella <bencap22@yahoo.com>
2020-04-27 12:51:53 -0400

    My first commit in Git

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * CSE 271
+ * Lab 13
+ * 4.27.20
+ * @author benca
+ * this is the class for the repository on gitHub
+ */
+public class HelloWorld {
+	
+	/**
+	 * this is the main method where the line is printed
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
